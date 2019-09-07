3ee7a50293f01be29b4363c19b6be5a6a819b053
Sam Yong <yongmc.sam2014@outlook.com>
2019-09-07 09:08:20 -0400

    separate ops

--- lambda_function.py
+++ lambda_function.py
@@ -2,11 +2,29 @@ import boto3
 import urllib
 
 s3 = boto3.resource('s3')
+bucket = s3.Bucket('storagepennapps19')
 
-def lambda_handler(event, context):
-    source = event['source']
-    key = event['key']
+def put(key, obj):
+    bucket.put_object(Key=key, Body=obj)
+
+def download_source(source):
     response = urllib.request.urlopen(source)
     data = response.read()
-    print(data)
-    s3.Bucket('storagepennapps19').put_object(Key=key, Body=data)
+    return data
+
+# op - 'puturl'
+#   source - url to get object
+#   key - to store object in bucket
+# op - 'get'
+#   key - stored object in bucket
+def lambda_handler(event, context):
+    op = event['op']
+    payload = event['payload']
+    if op == 'puturl': # put object from url
+        source = payload['source']
+        key = payload['key']
+        data = download_source(source)
+        put(key, data)
+        return 'ok'
+    if op == 'get': # get object from bucket
+        pass
